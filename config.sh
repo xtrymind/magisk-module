16bc8c7b1ca42cff44dae101a91a5808cce7c1b1
Dede Dindin Qudsy <xtrymind@gmail.com>
2018-12-06 14:00:09 +0700

    initial config
    Signed-off-by: Dede Dindin Qudsy <xtrymind@gmail.com>

--- config.sh
+++ config.sh
@@ -41,7 +41,7 @@ LATESTARTSERVICE=false
 
 print_modname() {
   ui_print "*******************************"
-  ui_print "     Magisk Module Template    "
+  ui_print " Wlan config for ZF Max Pro M1 "
   ui_print "*******************************"
 }
 
